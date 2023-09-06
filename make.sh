339a8bd31023fc27fb751ac3d455262c947a9bea
Novák, Stanislav <novaks11@fel.cvut.cz>
2023-09-07 01:06:48 +0200

    1.2gama, sheet

--- make.sh
+++ make.sh
@@ -1,3 +1,7 @@
 ls song/*.tex | awk '{printf "\\input{%s}\n", $$1}' > inputs.tex
+ls noty/*.tex | awk '{printf "\\input{%s}\n", $$1}' > inputs-noty.tex
 pdflatex sample-sb
 pdflatex sample-sb
+pdflatex changelog
+pdflatex noty
+pdflatex noty
